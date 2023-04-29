59ecbdeb74f0b4873636f681a2bd4c58804c96a0
mysnowtest2023 <132148820+mysnowtest2023@users.noreply.github.com>
2023-04-29 13:15:05 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,4 +3,6 @@ import requests
 streamlit.title('My healthy new diner!')
 streamlit.header('Fruity Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.jason())
+streamlit.text(fruityvice_response)
+#streamlit.text(fruityvice_response.jason())
+#fruityvice_normalized = pandas.jason_normalize(fruityvice_response.jason())
