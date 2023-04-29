f6cf78ee71e9b5bcd21e9d0528c97c13e2468757
mysnowtest2023 <132148820+mysnowtest2023@users.noreply.github.com>
2023-04-29 14:49:14 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,5 +4,6 @@ streamlit.title('My healthy new diner!')
 streamlit.header('Fruity Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#streamlit.dataframe(fruityvice_normalized)
+import snowflake.connector
