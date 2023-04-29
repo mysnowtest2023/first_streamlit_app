15f59b8f5674b901a7249608a0b101a8170201df
mysnowtest2023 <132148820+mysnowtest2023@users.noreply.github.com>
2023-04-29 13:06:45 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,5 @@
 import streamlit
-import request
+import requests
 streamlit.title('My healthy new diner!')
 streamlit.header('Fruity Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
